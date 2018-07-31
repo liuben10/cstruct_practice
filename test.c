a19a325ce3b38e2da31368382242361974bbe42f
Benjamin Liu <liuben10@fb.com>
2018-07-30 20:02:36 -0700

    Made sha256 compile, also addeded some experiments with Test

--- test.c
+++ test.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DM(x, y) x y;
+
+#define CLASS(T)					\
+  struct T *new_##T() {					\
+    T *result = malloc(sizeof(T));			\
+    result->this = result;				\
+    result->new = &new_##T;				\
+    return result;					\
+  }							\
+
 typedef struct Tuple {
-  void *first;
+  int x;
+  int y;
+  struct Tuple *this;
+  struct Tuple *(*new)();
 } Tuple;
 
-int main() {
-  Tuple *t = malloc(sizeof(Tuple));
-  t->first = malloc(sizeof(char));
-  ((char *) t->first)[0] = 'c';
-  char *pulled = (char *)t->first;
-  printf("%s\n", t->first);
-  return 0;
+CLASS(Tuple);
+
+int main(){
+  Tuple *result = new_Tuple();
+  result->x = 10;
+  result->y = 11;
+  printf("x=%d, y=%d\n", result->x, result->y);
 }
